f9315a165ff7606468f9324d32cbbe1644aea8d8
Shavarsh <shavarsh.abrahamyan1993@gmail.com>
2020-02-25 15:37:08 +0400

    deleted unnneccessary simpleButton component

--- src/components/simpleButton.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Button from "antd/es/button";
-
-export default function SimpleButton(props) {
-    return (
-        <Button type="primary">Primary</Button>
-    )
-}
