7dea863c89e350ba80cb22329c85f8bec982bf59
Ani <ani_gevorgyan2@edu.aua.am>
2020-02-26 14:29:16 +0400

    services

---
+++ src/pages/registration/services/services.js
@@ -0,0 +1,31 @@
+import axios from "axios"
+
+export function signUp(url, data) {
+    axios.post(`${url}`, {data})
+        .then(res => {
+            console.log(res);
+            console.log(res.data);
+        })
+        .catch(e => console.log(e.message))
+}
+
+export function signIn(url, data) {
+    axios.post(`${url}`, {data})
+        .then(res => {
+            console.log(res);
+            console.log(res.data);
+        })
+        .catch(e => console.log(e.message))
+}
+
+export function getCompanies(url) {
+    axios.get(`${url}`)
+        .then(res => {
+            console.log(res);
+            console.log(res.data);
+        })
+        .catch(e => console.log(e.message))
+}
+
+
+
