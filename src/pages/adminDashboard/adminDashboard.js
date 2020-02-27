08d43114f901f2cb9642f0f5e40855bec107bed4
Ani <ani_gevorgyan2@edu.aua.am>
2020-02-27 17:32:12 +0400

    fix: home page style

--- src/pages/adminDashboard/adminDashboard.js
+++ src/pages/adminDashboard/adminDashboard.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
 import { Table } from 'antd';
+import { getCompanies } from '../registration/services/services';
 
 const columns = [
+    {
+
+    },
     {
         title: 'Name',
         dataIndex: 'name',
     },
     {
-        title: 'last name',
+        title: 'Last name',
         dataIndex: 'lastName',
     },
     {
         title: 'phone',
         dataIndex: 'phone',
     },
+    {
+        title: 'Tax Number',
+        dataIndex: 'taxNumber'
+    },
+    {
+        title: 'Photo',
+        dataIndex: 'photo'
+    },
+    {
+        title: 'User Type',
+        dataIndex: 'userType'
+    },
+    {
+        activity: 'Activity',
+        dataIndex: 'activity'
+    }
 ];
-
 
 
 const dataSource = [];
