139e1a2795306ac39ed7327ca4a2e47bded4e740
Shavarsh <shavarsh.abrahamyan1993@gmail.com>
2020-03-02 18:54:08 +0400

    feat(/companyProfile):added companyProfile page, connected with redux, getting company data from redux, after sign in

--- src/pages/profilePage/ProfilePage.js
+++ src/pages/profilePage/ProfilePage.js
@@ -1,45 +1,72 @@
-import React, { useEffect } from "react";
-import { Layout, Menu, Breadcrumb } from "antd";
+import React, {useEffect} from "react";
+import {Layout, Menu, Breadcrumb, Descriptions} from "antd";
 import "./profilePage.css";
-import { getCookie } from "../registration/services/cookies";
+import {getCookie} from "../registration/services/cookies";
 import history from "../../routes/history";
+import {currentCompany} from "../../redux/actions";
+import {connect} from "react-redux";
+import Collapse from "antd/es/collapse";
+import List from "antd/es/list";
+import ListItem from "antd/es/transfer/ListItem";
+const { Panel } = Collapse;
 
-const { Header, Content, Footer } = Layout;
+const {Header, Sider, Content, Footer} = Layout;
 
-export default function ProfilePage(props) {
-  useEffect(() => {
-    if (getCookie("token")) {
-      history.push("/profile");
-    } else {
-      history.push("/");
+function ProfilePage(props) {
+
+    function callback(key) {
+        console.log(key);
     }
-  }, []);
 
-  return (
-    <Layout className="layout" style={{ minHeight: "100vh" }}>
-      <Header>
-        <div className="logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={["2"]}
-          style={{ lineHeight: "64px" }}
-        >
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
-        </Menu>
-      </Header>
-      <Content style={{ padding: "0 50px" }}>
-        <Breadcrumb style={{ margin: "16px 0" }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>Profile</Breadcrumb.Item>
-        </Breadcrumb>
-        <div className="site-layout-content">This is a profile page</div>
-      </Content>
-      <Footer style={{ textAlign: "center" }}>
-        Simply ©2020 Created by Frontend developers
-      </Footer>
-    </Layout>
-  );
+    useEffect(() => {
+        console.log(props)
+        if (!getCookie("token")) {
+            history.push("/");
+        }
+    }, []);
+
+    const text = "hcvsskfvbjdbvhjbdjhbhjbjhbhjb";
+    const {currentCompany} = props;
+
+
+
+    return (
+        <>
+            <Sider theme="light" width={350}>
+                {Object.keys(currentCompany).map((el) => {
+                    return(
+                        <List key={el}>
+                            <List.Item><strong>{`${el} :`}</strong>{` ${currentCompany[el]}`}</List.Item>
+                        </List>
+                    )
+                })}
+            </Sider>
+            <Layout>
+                <Header>
+                   <div>Header</div>
+                </Header>
+            </Layout>
+        </>
+    );
 }
+
+function mapStateToProps(state) {
+    return {
+        currentCompany: state.companies.currentCompany,
+        allCompanies: state.companies.allCompanies,
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        addCurrentCompany: (company) => {
+            dispatch(currentCompany(company))
+        },
+    }
+}
+
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProfilePage);
+
