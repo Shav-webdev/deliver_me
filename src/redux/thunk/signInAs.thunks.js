9956f02b384c6c157b4143923395322e37914720
Ani <ani_gevorgyan2@edu.aua.am>
2020-03-06 15:18:54 +0400

    fix: error messages

--- src/redux/thunk/signInAs.thunks.js
+++ src/redux/thunk/signInAs.thunks.js
@@ -6,24 +6,33 @@ import {
   signInAsUserSuccess,
 } from '../action'
 import history from '../../routes/history'
-import { setCookie } from '../../pages/registration/services/cookies'
+import {
+  setCookie
+} from '../../pages/registration/services/cookies'
+import {
+  errorMessage,
+  successMessage,
+} from '../../pages/registration/services/services'
 
 export const signInAs = data => async dispatch => {
   try {
     dispatch(signInAsCompanyRequest())
     const response = await api.login.post(data)
     if (response.status !== 200) {
+      console.log(response)
+      errorMessage(response.data.message)
       throw new Error('Something went wrong, try again')
     }
     if (response.data.type === 'company') {
       setCookie('token', `${response.data.token}`)
       setCookie('id', `${response.data.id}`)
+      successMessage('Sign In is successful !')
       history.push('/profile/company')
     } else if (response.data.type === 'user') {
       dispatch(signInAsUserSuccess(response.data))
       setCookie('token', `${response.data.token}`)
       setCookie('id', `${response.data.id}`)
-
+      successMessage('Sign In is successful !')
       history.push('/profile/user')
     } else {
       dispatch(signInAsUserSuccess(response.data))
