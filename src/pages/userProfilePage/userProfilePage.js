0ccd5ff073e4d603a6c7d63666ca1f3b1ecb222c
Ani <ani_gevorgyan2@edu.aua.am>
2020-03-06 11:47:10 +0400

    add user profile imports

--- src/pages/userProfilePage/userProfilePage.js
+++ src/pages/userProfilePage/userProfilePage.js
@@ -1,22 +1,48 @@
-import React, { useState, useEffect } from 'react'
-import { Table, Typography, Popover } from 'antd'
+import React, { useEffect, useState, useCallback } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import {
-    MenuFoldOutlined,
-    MenuUnfoldOutlined,
-    EditFilled,
-    CheckCircleFilled,
-    CloseCircleFilled,
-    DeleteFilled,
-} from '@ant-design/icons'
-import { Layout, Menu, Icon } from 'antd'
+    Button,
+    Icon,
+    Layout,
+    Typography,
+    message,
+    Upload,
+    Form,
+    Input,
+    Avatar,
+    DatePicker,
+    Card,
+    Collapse,
+} from 'antd'
+import './profilePage.css'
+import { getCookie } from '../registration/services/cookies'
+import history from '../../routes/history'
+import List from 'antd/es/list'
+import company_avatar from '../../assets/images/company_avatar.png' //change to user avatar
+import axios from 'axios'
+import Popover from 'antd/es/popover'
+import { logOut } from '../registration/services/services'
+import Modal from 'antd/es/modal'
+import Menu from 'antd/es/menu'
+import moment from 'moment'
 import Spinner from '../../components/spiner/spinner'
 import { connect } from 'react-redux'
 import {
     getUserAllTakenOrdersThunk,
     getUserAllActiveOrdersThunk
 } from '../../redux/thunk'
+import activeOrdersIcon from '../../assets/images/activeOrdersIcon.svg'
+import allOrdersIcon from '../../assets/images/allOrdersIcon.svg'
+import doneOrdersIcon from '../../assets/images/doneOrdersIcon.svg'
+import logo from '../../assets/images/logo.svg'
+import {
+    validateEmail,
+    validateName,
+    validateAddress,
+    validatePhoneNumber
+} from '../../pages/registration/helpers/validations'
 const { Header, Sider, Content } = Layout
-const { Column } = Table
+
 
 const defaultState = {
     collapsed: false,
